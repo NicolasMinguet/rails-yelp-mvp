2d05e8b595712940d474d704ca6c48a8417c4ef9
Nicolas Minguet <nicolas.minguet@hotmail.fr>
2019-05-22 16:19:24 +0200

    great restaurant and reviews

---
+++ app/models/review.rb
@@ -0,0 +1,8 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :rating, numericality: { only_integer: true }
+  validates :content, :rating, :restaurant, presence: true
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'cleaning database'
+Restaurant.destroy_all
+
+puts 'creating new restaurants'
+10.times do
+  category = ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  Restaurant.create!(
+    name: Faker::JapaneseMedia::OnePiece.character,
+    address: Faker::Movies::HarryPotter.location,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: category
+  )
+end
+
+puts 'created 10 great restaurants'
